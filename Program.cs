9723759a41d25521842e5e9dcb61cfb1ded08279
João Vitor Brandão Redigolo <136504184+Oajjao@users.noreply.github.com>
2025-08-25 13:54:49 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+public class Programa1
+{ 
+    public static void Main()
+    {
+        string nome = "Joao";
+        string sobrenome = " Brandao";
+        string str = nome + sobrenome;
+        Console.WriteLine(str);
+    }
+}
