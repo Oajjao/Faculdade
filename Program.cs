04b5103d53d93c3abf79f5d39f7657d747a54f49
João Vitor Brandão Redigolo <136504184+Oajjao@users.noreply.github.com>
2025-08-27 10:19:19 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string nome = Console.ReadLine();
+        string nome = "João";           //o aquivo string precisa de "" em vez de ''
+        float valorProduto = 29.99f;    //valores decimais precisao ser float em vez de int e também adicionar o f no final
+        char resposta = 'S';            //o aquivo char precisa de '' em vez de ""
 
-        Console.WriteLine("Bem vindo " + nome);
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Valor do produto: {valorProduto}");
+        Console.WriteLine($"Resposta: {resposta}");
+        Console.WriteLine("\n");
     }
 }
